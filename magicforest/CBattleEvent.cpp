3cbd39d5f6f20d689711d251b5e1d6e9a5bfd13a
Marko Silokunnas <marko.silokunnas@gmail.com>
2009-12-18 15:20:45 +0200

    Made opponent strike back

--- magicforest/CBattleEvent.cpp
+++ magicforest/CBattleEvent.cpp
@@ -1,5 +1,7 @@
 #include "CBattleEvent.h"
+
 #include <iostream>
+#include "CGameCharacter.h"
 
 CBattleEvent::CBattleEvent()
 {
@@ -10,7 +12,7 @@ CBattleEvent::CBattleEvent()
 CBattleEvent::CBattleEvent(CGameCharacter* pPlayer, CGameCharacter* pOpponent)
 {
   this->Player = pPlayer;
-  this->pOpponent = pOpponent;
+  this->Opponent = pOpponent;
 }
 
 /*
@@ -61,18 +63,22 @@ int CBattleEvent::RunEvent()
     return 0;
   }
   
-  // Set targets so we can use AttackTarget() function
+  // Set targets so we can use the AttackTarget() function
   Player->SetTarget(Opponent);
   Opponent->SetTarget(Player);
-
+  
   std::cout << "You're fighting against " << Opponent->GetName() << std::endl;
 
   while( FightIsOn )
   {
     int choice = 0;
 
-    while( choice < 1 && choice > 3 )
+    while( choice < 1 || choice > 3 )
     {
+      std::cout << "Your opponent has " << Opponent->GetHP() << "HP left";
+      std::cout << std::endl;
+      std::cout << "You have " << Player->GetHP() << "HP left";
+      std::cout << std::endl;
       std::cout << "What are you going to do?" << std::endl;
       std::cout << "1. Strike your foe down!" << std::endl;
       std::cout << "2. Run away like a little girl and cry yourself to sleep!";
@@ -84,8 +90,11 @@ int CBattleEvent::RunEvent()
 
     if( choice == 1 )
     {
-      std::cout << "YEAH!! You dealt " << Player->AttackTarget << " damage to ";
+      std::cout << "YEAH!! You dealt " << Player->AttackTarget() << " damage to ";
       std::cout << Opponent->GetName() << std::endl; 
+
+      std::cout << Opponent->GetName() << " Deals " << Opponent->AttackTarget();
+      std::cout << " damage to you!" << std::endl;
     }
 
     // haven't really thought escaping through yet so we'll just insult the 
@@ -93,23 +102,23 @@ int CBattleEvent::RunEvent()
     else if( choice == 2 )
     {
       std::cout << "YOU COWARD! Get back in the there!" << std::endl;
-      std::cout << Opponent->GetName() << "Will get a free strike at you!";
+      std::cout << Opponent->GetName() << " Will get a free strike at you!";
       std::cout << std::endl;
-      std::cout << Opponent->GetName() << "Deals " << Opponent->AttackTarget();
+      std::cout << Opponent->GetName() << " Deals " << Opponent->AttackTarget();
       std::cout << " damage to you!" << std::endl;
     }
 
-    if( Player->GetHP() == 0 )
+    if( Opponent->GetHP() == 0 )
     {
-      std::cout << "You died :(" << std::endl;
-      std::cout << "You weren't able to make it out of the magic forest";
+      std::cout << "Congratulations! You killed " << Opponent->GetName();
       std::cout << std::endl;
       FightIsOn = false;
     }
 
-    else if( Opponent->GetHP() == 0 )
+    else if( Player->GetHP() == 0 )
     {
-      std::cout << "Congratulations! You killed " << Oppponent->GetName();
+      std::cout << "You died :(" << std::endl;
+      std::cout << "You weren't able to make it out of the magic forest";
       std::cout << std::endl;
       FightIsOn = false;
     }
